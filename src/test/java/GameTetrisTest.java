111af8933c7f98169b6da86673491053c6649b63
basilissk <52831018+basilissk@users.noreply.github.com>
2021-12-15 01:05:14 +0300

    Add files via upload

---
+++ src/test/java/GameTetrisTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTetrisTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void frameInit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createRootPane() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void processWindowEvent() {
+    }
+}
