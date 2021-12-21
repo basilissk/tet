5c893c0500a87bba3a4d75072db5dec3b2368213
basilissk <52831018+basilissk@users.noreply.github.com>
2021-12-21 22:25:48 +0300

    Update GameTetrisTest.java

--- src/test/java/GameTetrisTest.java
+++ src/test/java/GameTetrisTest.java
@@ -1,24 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-class GameTetrisTest {
-
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+public class GameTetrisTest {
+    @Test
+    public void isComplexCalculations1WorksCorrectly(){
+        Assert.assertEquals(125, GameTetris.complexCalculations1(125));
     }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    @Test
+    public void isComplexCalculations2WorksCorrectly(){
+        Assert.assertEquals(125, GameTetris.complexCalculations2(125));
     }
-
-    @org.junit.jupiter.api.Test
-    void frameInit() {
+    @Test
+    public void isComplexCalculations3WorksCorrectly(){
+        Assert.assertEquals(125, GameTetris.complexCalculations3(125));
     }
-
-    @org.junit.jupiter.api.Test
-    void createRootPane() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void processWindowEvent() {
+    @Test
+    public void isComplexCalculations4WorksCorrectly(){
+        Assert.assertEquals(125, GameTetris.complexCalculations4(125));
     }
 }
